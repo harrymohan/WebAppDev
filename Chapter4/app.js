bf67045f5fcb6f840af1ad6b80f0ae5ffc8db445
unknown <erharmohan@gmail.com>
2014-04-20 21:34:28 -0700

    Jquery code

--- Chapter4/app.js
+++ Chapter4/app.js
@@ -2,6 +2,22 @@ var main = function()
 {
 	"use strict";
 
+$(".comment-input input").on("keypress",function(event){
+	if(event.keyCode==13){
+		var $new_comment = $("<p>");
+ 	//$new_comment.text($(".comment-input input").val());
+ 	//$(".comments").append($new_comment);
+ 	if($(".comment-input input").val() !== "")
+ 	{
+ 		$new_comment = $("<p>").text($(".comment-input input").val());
+ 		$new_comment.hide();
+ 		$(".comments").append($new_comment);
+ 		$new_comment.fadeIn();
+ 		$(".comment-input input").val("");
+ 	}
+	}
+});
+
  $(".comment-input button").on("click",function(event){
  	//append the comment
  	var $new_comment = $("<p>");
@@ -11,8 +27,10 @@ var main = function()
  	{
  		$new_comment = $("<p>").text($(".comment-input input").val());
  		$(".comments").append($new_comment);
+ 		$(".comment-input input").val("");
  	}
  });
+
 };
 
 $(document).ready(main);
